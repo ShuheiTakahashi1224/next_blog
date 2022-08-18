7ed8c1202963864d02bfe743d38d996d2b88cf15
ShuheiTakahashi1224 <shuheytakahashi@gmail.com>
2022-08-18 10:49:42 +0900

    eyecatch 設定

--- pages/blog/schedule.jsx
+++ pages/blog/schedule.jsx
@@ -11,6 +11,8 @@ import ConvertBody from 'components/ConvertBody'
 import PostCategories from 'components/PostCategories'
 import { extractText } from 'lib/extractText'
 import Meta from 'components/Meta'
+import { eyecatchLocal } from 'lib/constans'
+import { getPlaiceholder } from 'plaiceholder'
 
 const Schedule = ({
   title,
@@ -40,6 +42,8 @@ const Schedule = ({
             height={eyecatch.height}
             sizes="(min-width: 1152px) 1152px, 100vw"
             priority
+            placeholder="blur"
+            blurDataURL={eyecatch.blurDataURL}
           />
         </figure>
         <TwoColumn>
@@ -58,15 +62,18 @@ const Schedule = ({
 }
 
 export async function getStaticProps() {
-  const slug = 'schedule'
+  const slug = 'micro'
   const post = await getPostBySlug(slug)
   const description = extractText(post.content)
+  const eyecatch = post.eyecatch ?? eyecatchLocal
+  const { base64 } = await getPlaiceholder(eyecatch.url)
+  eyecatch.blurDataURL = base64
   return {
     props: {
       title: post.title,
       publish: post.publishDate,
       content: post.content,
-      eyecatch: post.eyecatch,
+      eyecatch: eyecatch,
       categories: post.categories,
       description: description,
     },
