885cdaf1f0a50815091831741604de1c76257c9b
ShuheiTakahashi1224 <shuheytakahashi@gmail.com>
2022-08-24 18:44:51 +0900

    top 投稿一覧表示

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,36 @@
 import Container from 'components/Container'
 import Hero from 'components/Hero'
 import Meta from 'components/Meta'
+import Pagination from 'components/Pagination'
+import Posts from 'components/Posts'
+import { getAllPosts } from 'lib/api'
+import { eyecatchLocal } from 'lib/constans'
+import { getPlaiceholder } from 'plaiceholder'
 
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <Container>
       <Meta />
       <Hero title="CUBE" subTitle="アウトプットしていくサイト" imageOn />
+      <Posts posts={posts} />
+      <Pagination nextUrl="/blog" nextText="More Posts" />
     </Container>
   )
+}
+
+export async function getStaticProps() {
+  const posts = await getAllPosts(4)
+
+  for (const post of posts) {
+    if (!post.hasOwnProperty('eyecatch')) {
+      post.eyecatch = eyecatchLocal
+    }
+    const { base64 } = await getPlaiceholder(post.eyecatch.url)
+    post.eyecatch.blurDataURL = base64
+  }
+  return {
+    props: {
+      posts: posts,
+    },
+  }
 }
